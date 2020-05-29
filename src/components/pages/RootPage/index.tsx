91ef38c03ec2639a7145f5d9a3a5b7b0708c3a14
Ushinji <siguma971@gmail.com>
2020-05-30 03:47:35 +0900

    Setup styled-components

--- src/components/pages/RootPage/index.tsx
+++ src/components/pages/RootPage/index.tsx
@@ -1,7 +1,14 @@
 import * as React from 'react';
+import styled from 'styled-components';
+
+const LContainer = styled.div`
+  font-size: 16px;
+  color: red;
+  line-height: 1.5;
+`;
 
 const RootPage: React.FC = () => {
-  return <div>Hello World!</div>
+  return <LContainer>Hello World!</LContainer>
 }
 
 export default RootPage;
