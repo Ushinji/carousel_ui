57f9736be94b257968be13ec4852c957aef8a34d
Ushinji <siguma971@gmail.com>
2020-05-30 04:38:05 +0900

    Add Carousel sample

--- src/components/pages/RootPage/index.tsx
+++ src/components/pages/RootPage/index.tsx
@@ -1,14 +1,41 @@
 import * as React from 'react';
 import styled from 'styled-components';
+import Carousel from '../../molecules/Carousel';
 
 const LContainer = styled.div`
   font-size: 16px;
-  color: red;
   line-height: 1.5;
+  text-align: center;
+`;
+
+const LBody = styled.div`
+  width: 840px;
+  margin: 0 auto;
+`;
+
+const LPage = styled.div`
+  background-color: gainsboro;
+  font-size: 32px;
+  font-weight: bold;
+  height: 240px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const RootPage: React.FC = () => {
-  return <LContainer>Hello World!</LContainer>
+  return (
+    <LContainer>
+      <h1>Carousel Sample</h1>
+      <LBody>
+        <Carousel>
+          <LPage>1</LPage>
+          <LPage>2</LPage>
+          <LPage>3</LPage>
+        </Carousel>
+      </LBody>
+    </LContainer>
+  )
 }
 
 export default RootPage;
