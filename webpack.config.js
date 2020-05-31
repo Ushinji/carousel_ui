6a5716f9e0a5b785fd773c02ab4392c47f17357a
Ushinji <siguma971@gmail.com>
2020-05-31 09:51:48 +0900

    Add build command

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,6 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  mode: 'development',
   entry: './src/index.tsx',
   output: {
     path: path.resolve(__dirname, './dist'),
